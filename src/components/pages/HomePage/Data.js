51f7478bbfd121a02d42be91bed91186a7142049
dynnah <dynnah_hanna@hotmail.com>
2020-11-26 12:10:50 -0300

    homepage

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Cirurgias de Castração',
+    headline: 'Centro Zoonoses',
+    description : 'Cadastre seu pet e agende a castração de forma rápida e fácil',
+    buttonLabel: 'Faça seu Cadastro',
+    imgStart: '',
+    img: 'images/logo.png',
+    alt: 'Zoonoses'
+}
+
+export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Cadastro e agendamento de forma fácil e segura para você e seu pet',
+    headline: '100% de Segurança',
+    description: 'O Centro de Vigilância Ambiental e Zoonoses realiza o agendamento para a realização da castração de cães de gatos.',
+    buttonLabel: 'Saiba Mais',
+    imgStart: 'start',
+    img: 'images/seguranca.png',
+    alt: 'Saiba Mais'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: 'O agendamento é fácil e simples',
+    headline: 'Sinônimo de Organização',
+    description: "Cadastre suas informações e as informações do seu pet de forma rápida e nós organizamos a cirurgia e entramos em contato com você com todas as informações",
+    buttonLabel: 'Faça seu Cadastro',
+    imgStart: '',
+    img: 'images/organizacao.png',
+    alt: 'Cadastro'
+  };
+  
+  export const homeObjFour = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Ainda tem alguma dúvida?',
+    headline: 'Entre em contato conosco',
+    description: 'Esclarecemos todas as suas dúvidas para que você se sinta seguro e confiante na realização da castração do seu pet',
+    buttonLabel: 'Entre em contato',
+    imgStart: 'start',
+    img: 'images/contato.png',
+    alt: 'Contato'
+  };
